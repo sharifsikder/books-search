d1bd9077de201e51d1c3cc6bd68258ab72c1c64f
sharifsikder <sharif01742088325@gmail.com>
2021-09-02 10:56:47 +0600

    added

--- book.js
+++ book.js
@@ -1,12 +1,16 @@
 
  const allBooks = document.getElementById('all-book');
  const totalBooks = document.getElementById('total-book');
+ const spinner = document.getElementById('spinner');
+ spinner.style.display = 'none'
+
 const searchButton = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
     // searchField.value = '';
 
+    spinner.style.display = 'block'
     allBooks.textContent = '';
     totalBooks.textContent = '';
    
@@ -21,29 +25,30 @@ const searchButton = () => {
 
 const displayBook = (data) => {
     console.log(data)
-    const count = data.docs;
-    const counts = count.length;
-    const totalDiv = document.createElement('div');
-    totalDiv.innerHTML = `<h3 class="totals"> Total Books : ${counts}</h3>`;
-    totalBooks.appendChild(totalDiv);
+  
+   
+    spinner.style.display = 'none';
+        const count = data.numFound;
+        const counts = count;
+        const totalDiv = document.createElement('div');
+        totalDiv.innerHTML = `<h3 class="totals"> Total Results Found : ${counts}</h3>`;
+        totalBooks.appendChild(totalDiv);
     
-    
-
     const books = data.docs;
     console.log(books)
     books.forEach(book => {
         console.log(book)
        
-      
         const div = document.createElement('div');
         div.classList.add('card');
         div.classList.add('book')
-        div.innerHTML = `<img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top book-image" alt="...">
+        div.innerHTML = `<img src=" https://covers.openlibrary.org/b/id/${book.cover_i? book.cover_i:''}-M.jpg" class="card-img-top book-image" alt="...">
         <div class="card-body">
-          <h3 class="card-title title">${book.title? book.title:''}</h3>
-          <h5 class="card-text athur">Author Name : ${book.author_name? book.author_name:''}</h5>
+          <h5 class="card-title title"> Book Name : ${book.title? book.title:''}</h5>
+          <h5 class="card-text athur"> Author : ${book.author_name? book.author_name:''}</h5>
+          <p class="card-text publisher">Publisher : ${book.publisher? book.publisher:''}</p>
           <p class= "card-text publish">First Published : ${book.first_publish_year? book.first_publish_year: '' }</p>
-          <a href="#" class="btn btn-danger">SALE</a>
+          <a href="#" class="btn btn-danger">Details >></a>
        `;
        allBooks.appendChild(div)
     })
