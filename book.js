f699662c7b29a502100103a17adc178d78fdb357
sharifsikder <sharif01742088325@gmail.com>
2021-09-02 12:18:54 +0600

    styles

--- book.js
+++ book.js
@@ -1,17 +1,14 @@
-
+// id call //
  const allBooks = document.getElementById('all-book');
  const totalBooks = document.getElementById('total-book');
  const spinner = document.getElementById('spinner');
- spinner.style.display = 'none'
+ spinner.style.display = 'none';
 
 //search button click//
 const searchButton = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-if(searchText === ''){
-    console.log('click')
-}
 
     spinner.style.display = 'block'
     allBooks.textContent = '';
@@ -21,28 +18,25 @@ if(searchText === ''){
     console.log(url)
     fetch(url)
     .then(res => res.json())
-    .then(data => displayBook(data))
-    
-    
+    .then(data => displayBook(data))   
 }
 
-const displayBook = (data) => {
-    console.log(data)
-    
+// Total result found show //
+const displayBook = (data) => { 
     spinner.style.display = 'none';
         const count = data.numFound;
         const totalDiv = document.createElement('div');
         totalDiv.innerHTML = `<h3 class="totals"> Total Results Found : ${count}</h3>`;
         totalBooks.appendChild(totalDiv);
-    
+        
+    // All books show//
     const books = data.docs;
-    console.log(books)
+    // console.log(books)
     books.forEach(book => {
-        console.log(book)
-       
+        // console.log(book)
         const div = document.createElement('div');
         div.classList.add('card');
-        div.classList.add('book')
+        div.classList.add('book');
         div.innerHTML = `<img src=" https://covers.openlibrary.org/b/id/${book.cover_i? book.cover_i:''}-M.jpg" class="card-img-top book-image" alt="...">
         <div class="card-body">
           <h5 class="card-title title"> Book Name : ${book.title? book.title:''}</h5>
